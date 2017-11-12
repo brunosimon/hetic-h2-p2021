009be7d07842d6ec6e1734106d2eb4b0543e7c22
brunosimon <simon.bruno.77@gmail.com>
2017-11-12 15:04:02 +0100

    :memo: Exercices > Update

---
+++ exercices/cours-04-drumkit-g1/js/script.js
@@ -0,0 +1,31 @@
+const $drumkit = document.querySelector('.drumkit')
+const $buttons = Array.from($drumkit.querySelectorAll('.button'))
+const $sounds = Array.from($drumkit.querySelectorAll('audio'))
+
+for(const $button of $buttons)
+{
+    $button.addEventListener('mousedown', (event) =>
+    {
+        event.preventDefault()
+
+        playSound($button.dataset.sound)
+    })
+}
+
+document.addEventListener('keydown', (event) =>
+{
+    const $button = $buttons.find((element) => element.classList.contains(`key-${event.keyCode}`))
+
+    if($button)
+    {
+        playSound($button.dataset.sound)
+    }
+})
+
+const playSound = (soundName) =>
+{
+    const $sound = $sounds.find((element) => element.classList.contains(soundName))
+
+    $sound.currentTime = 0
+    $sound.play()
+}
